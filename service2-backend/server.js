f738146c9b6c198d48895feaac01ef7e5d54372f
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-08 09:51:45 -0300

    feat: simple backend node api

---
+++ service2-backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Service 2!');
+});
+
+app.get('/greet/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}! Welcome to Service 2.`);
+});
+
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.json({
+    message: 'Data received successfully!',
+    receivedData: data
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Service 2 is running`);
+});
+
+
