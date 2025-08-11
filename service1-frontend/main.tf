046c017b457258f634be1a5826ca7b44ad296902
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-11 17:27:21 -0300

    fix&update: depends public access block

--- service1-frontend/main.tf
+++ service1-frontend/main.tf
@@ -43,6 +43,7 @@ resource "aws_s3_bucket_policy" "public" {
             }
         ]
     })
+    depends_on = [aws_s3_bucket_public_access_block.public_access]
 }
 
 resource "aws_s3_object" "index" {
