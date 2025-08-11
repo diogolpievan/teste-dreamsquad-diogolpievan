3a814aaa48c71efc3bd2581b41ca45781e1d072f
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-11 12:13:58 -0300

    update: all outputs created

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output forntend_url {
+output frontend_url {
     description = "URL for Service 1 - Frontend"
-    value       = "Service 1 - Frontend: ${}" 
+    value       = "Service 1 - Frontend: ${module.frontend.website_url}" 
 }
 output backend_url {
     description = "URL for Service 2 - Backend"
-    value       = "" 
+    value       = "Service 2 - Backend: ${module.backend.backend_url}"
 }
 output job_bucket_name {
     description = "Bucket name used by Service 3 - Job"
-    value       = ""
+    value       = "Service 3 - Job: ${module.job.bucket_name}"
 }
