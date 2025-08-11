cd6576e72398c52d0e7372ebfb7e94dd9bb7e8b3
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-11 12:13:31 -0300

    fix: modules

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 module "frontend" {
     source      = "./service1-frontend"
-    bucket_name = "dreamsquad-frontend-site"
 }
 module "backend" {
     source      = "./service2-backend"
-    app_name    = "dreamsquad-backend-api"
 }
 module "job" {
     source      = "./service3-job"
-    bucket_name = "dreamsquad-job-output"
 }
