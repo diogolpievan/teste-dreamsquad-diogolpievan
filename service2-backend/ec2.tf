2a2ff1c4f7af8a16fe79acc79a62570dbb2416cb
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-08-11 12:10:49 -0300

    fix: path of script.sh

--- service2-backend/ec2.tf
+++ service2-backend/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "test-dreamsquad-diogolpievan" {
     ami                    = var.ami_id
     instance_type          = var.instance_type
-    vpc_security_group_ids = [aws_security_group.demoday-test-dreamsquad-diogolpievan-sg.id]
-    user_data              = file("script.sh")
+    vpc_security_group_ids = [aws_security_group.test-dreamsquad-diogolpievan-sg.id]
+    user_data              = file("${path.module}/script.sh")
 
     tags = {
         Name = "test-dreamsquad-diogolpievan"
